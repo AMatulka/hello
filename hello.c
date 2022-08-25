4659701d70916484a780bb481b11591a904f5486
Austin Matulka <austinmatulka@gmail.com>
2022-08-25 20:16:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Austin Matulka and Rally Grotelueschen
+ *         austinmatulka@unomaha.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Austin Matulka\n");
+
+  return 0;
+}
