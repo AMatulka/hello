8328faa6ae04cc3d219375148e7c0e426a88c845
Rally Grotelueschen <rallygrote@gmail.com>
2022-08-25 22:02:28 +0000

    added myself as a partner

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Austin Matulka!\n");
   printf("My Major is Electrical Engineering\n");
+  printf("I am Rally Grotelueschen"\n);
+  printf("My major is Computer Engineering"\n);
 
   return 0;
 }
